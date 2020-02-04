851202e1708f454c30b276a0c115f81137b46a14
sarkanyjudo <sarkanyjudo@gmail.com>
2020-02-03 21:51:46 -0800

    Connected to Contentful

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,18 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
+
 import Navbar from './Globals/Navbar'
+import Footer from './Globals/Footer'
+
 import './bootstrap.min.css'
 import "./layout.css"
 
 const Layout = ({ children }) => (
-  <>
+   <>
     <Navbar/>
     {children}
+    <Footer />
   </>
 )
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,17 +5,17 @@ import SEO from '../components/seo';
 import BackgroundSection from '../components/Globals/BackgroundSection';
 import Info from '../components/Home/Info';
 
-const IndexPage = ({ data }) => (
+const AboutPage = ({ data }) => (
 	<Layout>
 		<SEO title="Home" />
-		<BackgroundSection img={data.img.childImageSharp.fluid} title="regular joe's" styleClass="default-background" />
+		<BackgroundSection img={data.img.childImageSharp.fluid} title="about us" styleClass="about-background" />
 		<Info />
 	</Layout>
 );
 
 export const query = graphql`
 	{
-		img: file(relativePath: { eq: "default-background.jpeg" }) {
+		img: file(relativePath: { eq: "about-background.jpeg" }) {
 			childImageSharp {
 				fluid {
 					...GatsbyImageSharpFluid
@@ -25,4 +25,4 @@ export const query = graphql`
 	}
 `;
 
-export default IndexPage;
+export default AboutPage;
