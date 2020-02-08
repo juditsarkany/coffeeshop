ceb4e231b5524f312d0c4765ca9941e2806a7ef3
sarkanyjudo <sarkanyjudo@gmail.com>
2020-02-07 23:48:37 -0800

    Added Footer Info

--- src/components/Globals/Footer.js
+++ src/components/Globals/Footer.js
@@ -6,7 +6,13 @@ export default function Footer() {
 			<div className="container">
 				<div className="row">
 					<div className="col-10 mx-auto col-md-6 text-yellow text-center text-capitalize">
-						<h3>all rights reserved &copy;{new Date().getFullYear().toString()}</h3>
+						<h4>all rights reserved &copy;{new Date().getFullYear().toString()}</h4>
+						<p>
+							Site created by{' '}
+							<a className="footer-info" href="https://brillful.com">
+								Brillful
+							</a>
+						</p>
 					</div>
 				</div>
 			</div>
